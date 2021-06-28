8091b7383ce4a9c38290a47e2254d7b70ee718ee
SEOLMIN KWON <stat_kwon@naver.com>
2021-06-28 11:19:22 +0900

    feat: Memo api 설계

---
+++ src/main/java/com/sparta/week03/domain/MemoRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.week03.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+
+}
